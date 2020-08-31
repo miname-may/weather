f0a1082b617009f037b2b9def8784b45e20aebac
miname-may <qwpo8329@gmail.com>
2020-09-01 07:31:47 +0900

    geolocation를 이용해 사용자의 위치를 얻어 그 지역의 날씨를 알아보는 웹사이트

--- weather/src/main/webapp/WEB-INF/views/weather.jsp
+++ weather/src/main/webapp/WEB-INF/views/weather.jsp
@@ -8,14 +8,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>당신의 날씨</title>
-	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-	<script type="text/javascript" src="./resources/js/weather.js">
-
-
-	</script>
+	<title>우리 동네 날씨 알리미</title>
+	<link rel="icon" href="./resources/images/weatherFavicon.png"> 
+	<link rel="stylesheet" href="./resources/css/weather.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.9/css/weather-icons.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 </head>
 <body>
-	<input type="button" value="내 위치" onclick="askForCoords()">
+<div id="bodyDiv">
+		<div id="title">
+			<h1>오늘의 우리 동네 날씨는?</h1>
+		</div>
+	<div id="contents">
+		<p id="weather-img"></p>
+		<p id="mainWeather">오늘은 <span id="weather"></span></p>
+		<p id="temp"></p>
+		<p><span id="min-temp"></span> /
+		<span id="max-temp"></span></p>
+		<p id="wind"></p>
+		<p id="clouds"></p>
+	</div>
+	
+</div>
 </body>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script type="text/javascript" src="./resources/js/weather.js"></script>
 </html>
--- weather/src/main/webapp/resources/js/weather.js
+++ weather/src/main/webapp/resources/js/weather.js
@@ -1,53 +1,105 @@
-
-
-		const COORDS = "coords";
-		const API_KEY = "aadb6c3af3c2ef08af264e2f6d815498";
+	var weatheris = document.querySelector(".js-weather");
+	
+	var COORDS = "coords";
+	var API_KEY = "aadb6c3af3c2ef08af264e2f6d815498";
 		
-		function getWeather(lat, lng) {
-			fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + API_KEY + "&units=metric"
-					).then(function(response) {	/* then = 앞의 fetch 실행 후 실행 */
-						return response.json();
-					}).then(function(json) {
-						console.log(json);
-					})
-		}
+	function getWeather(lat, lng) {
+		fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + API_KEY + "&units=metric&lang=kr"
+				).then(function(response) {	/* then = 앞의 fetch 실행 후 실행 */
+					return response.json();
+				}).then(function(json) {
+					var temperature = json.main.temp;
+					var weather = json.weather[0].icon;
+					
+					document.querySelector("#temp").innerHTML = "현재 온도 <b>" + json.main.temp + "℃</b>";
+					document.querySelector("#min-temp").innerHTML = "최저 온도 <b>" + json.main.temp_min + "℃</b>";
+					document.querySelector("#max-temp").innerHTML = "최고 온도 <b>" + json.main.temp_max + "℃</b>";
+					document.querySelector("#weather").innerHTML = "<b>" + json.weather[0].description + "</b>";
+					document.querySelector("#wind").innerHTML = "바람 <b>" + json.wind.speed + "</b>m/s";
+					document.querySelector("#clouds").innerHTML = "구름 <b>" + json.clouds.all + "</b>%";
+					
+					switch (json.weather[0].icon) {
+					case "01d":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-day-sunny'></i>";
+						break;
+					case "01n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-night-clear'></i>";
+						break;
+					case "02d":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-day-cloudy'></i>";
+					case "02n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-night-alt-cloudy'></i>";
+						break;
+					case "03d": case "03n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-cloud'></i>";
+						break;
+					case "04d": case "04n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-cloudy'></i>";
+					console.log(json.weather[0].icon);
+						break;
+					case "09d": case "09n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-rain'></i>";
+						break;
+					case "10d":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-day-rain'></i>";
+						break;
+					case "10n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-night-alt-rain'></i>";
+						break;
+					case "11d": case "11n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-lightning'></i>";
+						break;
+					case "13d": case "13n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-snowflake-cold'></i>";
+						break;
+					case "50d": case "50n":	
+						document.querySelector("#weather-img").innerHTML = "<i class='icon-img wi wi-fog'></i>";
+						break;
+					}
+					
+				})
+	}
 		
-		function saveCoords(coordsObj) {
-			localStorage.setItem(COORDS, JSON.stringify(coordsObj));
-		}
-		
-		function loadCoords() {
-			const loadedCoords = localStorage.getItem(COORDS);
-			if(loadedCoords === null){
-				askForCoords();
-			}else{
-//				getWeather
-				const parseCoords = JSON.parse(loadCoords);
+	function saveCoords(coordsObj) {
+		localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+	}
+	
+	function loadCoords() {
+		var loadedCoords = localStorage.getItem(COORDS);
+		if(loadedCoords === null){
+			askForCoords();
+		}else{
+//			getWeather
+			const parseCoords = JSON.parse(loadCoords);
 				getWeather(parseCoords.latitude, parseCoords.longitude);
-			}
 		}
+	}
 		
-		function handleGeoSucces(position) {
-			const latitude = position.coords.latitude;
-			const longitude = position.coords.longitude;
-			const coordsObj = {	/* 좌표 저장 */
-					latitude,
-					longitude
-			};
-			saveCoords(coordsObj);
-			getWeather(latitude, longitude);
-		}
+	function handleGeoSucces(position) {
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		var coordsObj = {	/* 좌표 저장 */
+				latitude,
+				longitude
+		};
+		saveCoords(coordsObj);
+		getWeather(latitude, longitude);
+	}
 
-		function handleGeoError() {
-			console.log('위치를 읽어올 수 없습니다.');
-		}
+	function handleGeoError() {
+		console.log('위치를 읽어올 수 없습니다.');
+	}
 
-
-		function askForCoords() {
-			if('geolocation' in navigator) {
-				navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
-				} else {
-				  alert('GPS를 지원하지 않습니다.');
-				}
-		}
+	function askForCoords() {
+		if('geolocation' in navigator) {
+			navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+			} else {
+			  alert('GPS를 지원하지 않습니다.');
+			}
+	}
+	
+	function init() {
+		askForCoords();
+	}
+	init();
 		
